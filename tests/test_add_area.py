1804f5c27cfd60d6353ffb59e6196a7ee14dca4b
J-Nafisa <131007811+J-Nafisa@users.noreply.github.com>
2023-05-12 18:40:50 +0300

    test_add_area.py

--- tests/test_add_area.py
+++ tests/test_add_area.py
@@ -1,24 +1,31 @@
+import math
 import pytest
 from scr.Circle import Circle
 from scr.Rectangle import Rectangle
 from scr.Square import Square
 from scr.Triangle import Triangle
 
+
 def test_add_area_rectangle_square():
-    rectangle = Rectangle(5, 10)
-    square = Square(4)
-    assert rectangle.add_area(square) == 66
+    rectangle = Rectangle(2, 5)
+    square = Square(3)
+    assert rectangle.add_area(square) == 19
+
 
 def test_add_area_square_circle():
     square = Square(4)
-    circle = Circle(10)
-    assert square.add_area(circle) == pytest.approx(502.6548245743669)
+    circle = Circle(7)
+    assert square.add_area(circle) == pytest.approx(85.539)
+
 
 def test_add_area_circle_triangle():
-    circle = Circle(10)
-    triangle = Triangle(3, 4, 5)
-    assert circle.add_area(triangle) == pytest.approx(306.2831853071796)
+    circle = Circle(6)
+    triangle = Triangle(5, 12, 13)
+    assert circle.add_area(triangle) == pytest.approx(113)
 
+
+if __name__ == "__main__":
+    pytest.main()
 
 
 
